fb01d130ee61fdcebcba5f18838f4e823084983c
Mobashshir Hasan <mobashshir12@gmail.com>
2025-08-30 09:36:55 +0000

    conversion of dayatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+//let score = "33"
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber) //Output: NaN
+
+let isLoggedIn = "mobashshir"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//  1 = true; 0= flase
+//  "" = false
+//  mobashshir = true;
+
+let number = 33
+ let stringNumber = String(number);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
