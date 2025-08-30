5bd55da014d619d520c7437805fb22adff76d8ca
Mobashshir Hasan <mobashshir12@gmail.com>
2025-08-30 10:02:41 +0000

    conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,8 +18,27 @@ console.log(booleanIsLoggedIn);
 //  "" = false
 //  mobashshir = true;
 
-let number = 33
- let stringNumber = String(number);
- console.log(stringNumber);
- console.log(typeof stringNumber);
+// let number = 33
+//  let stringNumber = String(number);
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber);
+
+
+
+ //*********************** Operations******************* */
+
+//  let str1 = "hello"
+//  let str2 = " mobashshir"
+
+//  let str3 = str1 + str2
+//  console.log(str3);
+
  
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`)
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`)
