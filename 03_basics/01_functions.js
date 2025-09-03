066699086207e94ab784c90445ece459acd32a26
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-03 03:33:56 +0000

    updated

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,6 +40,38 @@ function loginUserMessage(username = "sam"){ //default value 'sam'
 }
 
 // console.log(loginUserMessage("MObashshir"))
-console.log(loginUserMessage(""))  //if we don't pass any value then it wil undefined
-console.log(loginUserMessage("Mobashshir"))  //if we pass any value then it will override the value of username
+// console.log(loginUserMessage(""))  //if we don't pass any value then it wil undefined
+// console.log(loginUserMessage("Mobashshir"))  //if we pass any value then it will override the value of username
 
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500))
+
+
+const user = {
+    username: "mobashshir",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 599
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
