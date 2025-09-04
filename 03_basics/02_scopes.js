b6cfb1674c7d7852a8fdcc5c7fbfeaae193336b9
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-04 03:50:39 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -15,3 +15,43 @@ if (true){
 console.log(a)
 // console.log(b)
 // console.log(c)
+
+
+function one(){
+    const username = "MObashshir"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+one()
+
+if(true){
+    const username = "MObashshir"
+    if (username === "MObashshir"){
+        const website = "Youtbue"
+        console.log(username + website);
+    }
+    // console.log(website); //not accesseble
+}
+// console.log(username); //not accesseble
+
+
+//+++++++++++++++++++++++++ interesting ++++++++++++
+
+function addone(num){
+    return num +1
+}
+
+addone(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
