0e6900433650669f8183f051ebd4f03a730ad165
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-05 16:19:40 +0000

    updated

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -14,9 +14,28 @@ const user = {
 
 // console.log(this);
 
-function chai(){
-    let username = "Mobashshir"
-    console.log(this );
-}
+// const chai = function (){
+//     let username = "Mobashshir"
+//     console.log(this.username );  //username can't be access here (undefined)
+// }
 
-chai()
+// chai()
+
+// const chai = () => {
+//     let username = "Mobashshir"
+//     console.log(this);
+// }
+// chai()
+
+// () => {}  // Arrow function
+
+// const addTwo = (num1 , num2) => {  //basic arrow function
+//     return num1 + num2
+// }
+// console.log(addTwo(3, 4))
+
+
+// const addTwo = (num1 , num2) => num1 + num2 //implicit return
+// const addTwo = (num1 , num2) => (num1 + num2) // if we don't use {} then no need to give return keyword
+const addTwo = (num1 , num2) => ({username: "Mobashshir"}) // returning the object
+console.log(addTwo(3, 5))
