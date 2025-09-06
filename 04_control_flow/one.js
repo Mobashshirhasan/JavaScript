11cc43e8d1f5dfdf8d7dea97851339d87c26f96c
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-06 04:53:33 +0000

    switch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,39 @@
+// if
+
+const isUserLoggedIn = true
+
+// if ( 2 === "2" ){
+//     console.log("Executed");
+// }
+
+// const  score = 200
+
+// if ( score > 100 ){
+//     let power = "fly"
+//     console.log(`user power ${power}`)
+// }
+
+// console.log(`user power ${power}`) //out of scope
+
+
+// const balance = 5000
+
+// if (balance <= 6000){
+//     console.log("less than 5000");
+// }
+// else{
+//     console.log("less than 6000")
+// }
+
+// const UserLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (UserLoggedIn && debitCard && 2==3){
+//     console.log("Allow to buy course");
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail){
+//     console.log("User logged in");
+// }
