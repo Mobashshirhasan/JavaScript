23303e784696838e16f6b3f6ba7d5a2db028e08c
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-06 13:11:40 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,23 @@
+const myObject = {
+    js: 'javascript',
+    cpp : 'c++',
+    rb: 'ruby',
+    swift: "swift by apple"
+}
+
+// for (const key in myObject){
+//     console.log(key);
+// }
+
+
+for (const key in myObject){
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+//for in loop
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming){
+    console.log(key);
+}
