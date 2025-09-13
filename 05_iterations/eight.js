5f37341895edf3ce7cec16bc1a32dabb97a5e737
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-13 14:23:48 +0000

    eight.js

---
+++ 05_iterations/eight.js
@@ -0,0 +1,31 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval){
+//     console.log(`acc: ${acc} and currval: ${currval}` );
+//     return acc + currval    
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "python course",
+        price: 999
+    },
+    {
+        itemName: "react course",
+        price: 599
+    },
+
+]
+
+const priceTopay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+console.log(priceTopay);
